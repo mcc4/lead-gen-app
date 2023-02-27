5f3103ce530fd8f2384d6a18ff4389e12c424e37
Mike Chase <mchase@linksquares.com>
2023-02-27 16:48:47 -0500

    add discord and google

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,19 +1,19 @@
 import NextAuth from "next-auth";
-// import DiscordProvider from "next-auth/providers/discord";
-// import GoogleProvider from "next-auth/providers/google";
+import DiscordProvider from "next-auth/providers/discord";
+import GoogleProvider from "next-auth/providers/google";
 import GithubProvider from "next-auth/providers/github";
 
 export const authOptions = {
 	// Configure one or more authentication providers
 	providers: [
-		// DiscordProvider({
-		// 	clientId: process.env.DISCORD_ID as string,
-		// 	clientSecret: process.env.DISCORD_SECRET as string,
-		// }),
-		// GoogleProvider({
-		// 	clientId: process.env.GOOGLE_ID as string,
-		// 	clientSecret: process.env.GOOGLE_SECRET as string,
-		// }),
+		DiscordProvider({
+			clientId: process.env.DISCORD_ID as string,
+			clientSecret: process.env.DISCORD_SECRET as string,
+		}),
+		GoogleProvider({
+			clientId: process.env.GOOGLE_ID as string,
+			clientSecret: process.env.GOOGLE_SECRET as string,
+		}),
 		GithubProvider({
 			clientId: process.env.GITHUB_ID as string,
 			clientSecret: process.env.GITHUB_SECRET as string,
